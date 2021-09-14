831c84cb6bd93feff019f057e1feee83141c91cb
Akshay Pandey <codeakshay31@gmail.com>
2021-09-14 20:45:31 +0530

    feat : faq section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LandingPage from '../components/LandingPage/LandingPage';
 import Workshops from '../components/Workshops/Workshops';
+import FaqComponent from '../components/Faq/faq';
 
 const Home: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ const Home: React.FC = () => {
       <LandingPage />
       <Workshops />
       <Workshops />
+      <FaqComponent />
     </div>
   );
 };
