46e65f2f50ffbb8feafe2e033eb954beaf6246d8
shubh asati <shubhtrikal9876@gmail.com>
2022-09-13 18:01:48 +0530

    update: mentor

--- src/components/Mentor.tsx
+++ src/components/Mentor.tsx
@@ -1,46 +1,42 @@
-import { Container, Avatar, Box, Grid } from '@material-ui/core';
-import React from 'react';
+import { Container, Avatar, Box, Grid } from "@material-ui/core";
+import React from "react";
 
 const Mentor = () => {
   return (
-    <Container maxWidth='lg' className='mentor-section'>
-      <Box className='title'>
+    <Container maxWidth="lg" className="mentor-section">
+      <Box className="title">
         <h2>
           <span>Know Your Mentor</span>
         </h2>
       </Box>
-      <Grid container justifyContent='center' alignItems='center'>
+      <Grid container justifyContent="center" alignItems="center">
         <Grid item xs={12} sm={8}>
-          <Box className='mentor-box'>
-            <Box className='mentor-img'>
+          <Box className="mentor-box">
+            <Box className="mentor-img">
               <Avatar
-                src='images/mentor-img.jpeg'
-                alt='mentor-img'
-                style={{ height: '140px', width: '140px' }}
+                src="images/mentor.jpeg"
+                alt="mentor-img"
+                style={{ height: "140px", width: "140px" }}
               />
             </Box>
-            <Box className='mentor-heading'>
+            <Box className="mentor-heading">
               <h2>Shantanu Shubham</h2>
-              {/* <h4>C.E.O. & Founder – Tech Cryptors</h4> */}
+              <h4>Leading Instructor at Coding Ninjas</h4>
             </Box>
-            <Box className='mentor-details'>
+            <Box className="mentor-details">
               <p>
-                We are elated to introduce Mr. Shantanu Shubham, C.E.O. and
-                Founder of Tech Cryptors as the mentor for Flair Haven this
-                year. He has been a research and development enthusiast since
-                2014 and has marvellously worked on different technologies and
-                trending software like Android Studio, PyCharm, MATLAB, Proteus
-                to name a few. His victories in developing various high-end
-                products are awe-inspiring. Conducting a vast number of
-                workshops, seminars, internship courses etc. and training
-                thousands of students from highly revered engineering colleges
-                like IITs & NITs, Mr. Shantanu Shubham has been greatly influential all
-                across India and deservingly is a member of Departmental
-                Advisory Boards (DAB) of many renowned colleges. His mastery and
-                passion for coding are, for sure, motivation and a source of
-                knowledge to everyone. Our mentor and we are more than excited
-                to meet the keen set of mentees and we look forward to a
-                fruitful and knowledgeable time.
+                With great contentment, we welcome Mr. Shantanu Shubham onboard
+                with us as the mentor of Flair Haven’22.He is currently one of
+                the top most instructors at Coding Ninjas and a software
+                engineer at Schlumberger. Having mentored over 2500 students, he
+                not only has all of us in awe of his excellent representational
+                and communication skills but has also proven his expertise in
+                various other domains like Web Development and technologies like
+                Spring Boot, Dropwizard, Django, React, Angular, React Native
+                and many more. His dynamic personality, grit for coding and mind
+                set are a constant motivation and inspiration for us all. Our
+                mentor together with the ISTE family is looking forward to
+                meeting the technocrat in you soon!!
               </p>
             </Box>
           </Box>
